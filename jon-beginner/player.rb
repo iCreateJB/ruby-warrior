fd5ff451b83b8b4e1e2d2b094207f38bb8080cc3
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-04-25 18:20:34 -0400

    Level 4

--- jon-beginner/player.rb
+++ jon-beginner/player.rb
@@ -1,9 +1,32 @@
 class Player
+    
+  def initialize
+    @max_health = 20
+  end
+
   def play_turn(warrior)
     if warrior.feel.enemy?
       warrior.attack!
     else
-      (warrior.health < 20) ? warrior.rest! : warrior.walk!
+      recover?(warrior)
+    end
+    record_damage(warrior)
+  end
+
+  def recover?(warrior)
+    if (warrior.health < @max_health) && !damaged?(warrior)
+      warrior.rest!
+    else
+      warrior.walk!
     end
   end
+
+  def damaged?(warrior)
+    warrior.health < @health
+  end
+
+  def record_damage(warrior)
+    @health = warrior.health
+  end
+
 end
